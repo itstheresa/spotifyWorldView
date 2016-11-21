9b7bae64efd34d2760e843938cb8f5784e14e905
itstheresa <phamk2@tcnj.edu>
2016-11-21 16:35:50 -0500

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  get '/albums' to: 'artists#index'
-  get '/artists/:id', to: 'artists#show', as: 'album'
+  get '/albums' to: 'albums#index'
+  get '/albums/:id', to: 'albums#show', as: 'album'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
