c84b1b3200ba88d74fa409b65ae8f0f770bb5786
itstheresa <phamk2@tcnj.edu>
2016-11-21 16:57:22 -0500

    Testing RSpotify integration

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,7 +1,12 @@
 class AlbumsController < ApplicationController
   def index
+	if !params[:album_name].empty?
+     @albums = RSpotify::Album.search(params[:album_name])
+    else
+     redirect_to root_path
   end
 
   def show
+  	@album = RSpotify::Album.find(params[:id])
   end
 end
