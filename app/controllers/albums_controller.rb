59b7415233c482036e91f69bc7d88947b6daafa8
itstheresa <phamk2@tcnj.edu>
2016-11-22 08:23:12 -0500

    Add last file edits

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,6 +2,8 @@ class AlbumsController < ApplicationController
   def index
   	if !params[:album_name].empty?
      @albums = RSpotify::Album.search(params[:album_name])
+	else
+     redirect_to root_path
     end
   end
 
