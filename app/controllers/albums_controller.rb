2b2341008a5bf3e5786d6fb63708e42d40564289
itstheresa <phamk2@tcnj.edu>
2016-11-24 19:58:16 -0500

    Added functionality linking search to map

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,13 +1,13 @@
 class AlbumsController < ApplicationController
-  def index
+  def index #controls what happens after search
   	if !params[:album_name].empty?
-     @albums = RSpotify::Album.search(params[:album_name])
+     @albums = RSpotify::Album.search(params[:album_name]) #utilizes Spotify search
 	else
-     redirect_to root_path
+     redirect_to root_path #if nothing is entered in search bar, nothing happens
     end
   end
 
-  def show
+  def show #finds specific album from Spotify that is selected after searching
   	@album = RSpotify::Album.find(params[:id])
   end
 end
