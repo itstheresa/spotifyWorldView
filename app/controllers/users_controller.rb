9e5d4aaffcb1e5f2ece7ec580913f322525afe5a
itstheresa <phamk2@tcnj.edu>
2016-12-15 16:41:25 -0500

    Added logout and more documentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
+# Part of New Functionality: Retrieves information after logging into Spotify
 class UsersController < ApplicationController
   def spotify
-  	@spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    # Now you can access user's private data, create playlists and much more
-	
-	#Returns albums saved in Spotify user's "Your Music" library
+  	# Allows access to user's data on Spotify
+  	spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+  	# Saves user so token can be automatically refreshed and user doesn't need to keep logging back in
+	hash = spotify_user.to_hash
+	@spotify_user = RSpotify::User.new(hash)
+	# Returns albums saved in Spotify user's "Your Music" library
 	@albums = @spotify_user.saved_albums
 
-  end
-
-  def action
   end
 end
