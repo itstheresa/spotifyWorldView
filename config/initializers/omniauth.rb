8bb3eb74beed974c40391ca309f0f6a576e05838
itstheresa <phamk2@tcnj.edu>
2016-12-16 10:02:25 -0500

    Updated scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 require 'rspotify/oauth'
 
-#includes client id, client secret, and the scope specified grants access to a user's "Your Music" library
+#includes client id, client secret, and the scopes specified grants access to a user's country as well as "Your Music" library
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, "b9e86650cc7a4733a7e24180f5b9014a", "e47d7214bf674d5586916f70b0bc68bd", scope: 'user-library-read'
+  provider :spotify, "b9e86650cc7a4733a7e24180f5b9014a", "e47d7214bf674d5586916f70b0bc68bd", scope: 'user-read-email user-library-read'
 end
